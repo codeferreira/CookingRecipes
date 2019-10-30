e4e19f773d8b8fcbc6cdcf7dbbc003015032d1b4
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-10-29 23:35:55 -0300

    chore: Add booked page

---
+++ src/pages/Booked/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Booked() {
+  return <View style={{backgroundColor: '#000', flex: 1}} />;
+}
