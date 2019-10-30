d9fb56a9398ada777fabb5d6e7911bb3e11cb86a
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-10-29 23:36:10 -0300

    chore: Add main page

---
+++ src/pages/Main/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Main() {
+  return <View style={{backgroundColor: '#FAFAFA', flex: 1}} />;
+}
