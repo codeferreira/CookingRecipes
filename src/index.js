6b22c1f2c011f21ca3ecf45198d6f31b4221b558
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-10-29 21:27:17 -0300

    chore: Add statusbar config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StatusBar, View} from 'react-native';
 
 // import { Container } from './styles';
 
 export default function App() {
-  return <View />;
+  return (
+    <>
+      <StatusBar backgroundColor="#FFF" barStyle="dark-content" />
+      <View />
+    </>
+  );
 }
