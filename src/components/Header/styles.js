0bbfc53a85ea640e799409e2ca08ae5bb73a9b92
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2019-10-30 22:54:59 -0300

    chore: Add Header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  margin-bottom: 16px;
+`;
+
+export const Title = styled.Text`
+  color: #bf8654;
+  font-size: 24px;
+  font-weight: bold;
+`;
